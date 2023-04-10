d3ade22873810449f32b6e24605e0ba7e33f2da4
digitalatz18 <130328002+digitalatz18@users.noreply.github.com>
2023-04-10 09:05:30 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyywjapvlv8cqhd4acclueug9t5p2keypepky7kg00xtqjkug3cuqqq7j0r3a -opmem -m 3 -r1 community-pools.mysrv.cloud:10300 -r 146.190.88.29:10100 -r1 nodent2.cpumining.cloud:10100 -p rpc;
+    ./astrominer -w dero1qyj68xc0gv62hzkz4vq5t84sk3uu6mmfaqhml339g0m0fa3pcnr5kqqvacekr -opmem -m 96 -r adnode.cpumining.cloud:10100 -r1 dero.cpumining.cloud:10100 -p rpc;
     sleep 5;
 done
